6de9eb7169af38263bf6a94ec9e68a69e707623e
gambino <lightsan52rus@yandex.ru>
2023-01-27 18:59:28 +0300

    Initial commit

---
+++ src/main/java/Tree.java
@@ -0,0 +1,58 @@
+
+import java.util.Objects;
+
+public class Tree {
+
+
+// Реализовать метод contains в классе Tree
+
+    public class tree<T extends Comparable<T>> {
+
+        private Node root;
+
+        private class Node {
+            private T value;
+            private Node left;
+            private Node right;
+
+            public Node(T value) {
+                this.value = value;
+            }
+        }
+
+        public void add(T value) {
+            Objects.requireNonNull(value);
+            root = appendNode(root, value);
+        }
+
+        private Node appendNode(Node current, T value) {
+            if (current == null) {
+                return new Node(value);
+            }
+
+            int compare = value.compareTo(current.value);
+            if (compare < 0) {
+                current.left = appendNode(current.left, value);
+            } else if (compare > 0) {
+                current.right = appendNode(current.right, value);
+            }
+
+            return current;
+        }
+
+        public boolean contains(T value) {
+            Node node = this.root;
+            while (node != null){
+                if (node.value.compareTo(value) == 0){
+                    return true;
+                }
+                if (node.value.compareTo(value) > 0) {
+                    node = node.left;
+                } else {
+                    node = node.right;
+                }
+            }
+            return false;
+        }
+    }
+}
